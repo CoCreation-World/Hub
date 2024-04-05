76f6db8bf266f9f5b748cdab90fd46e2bfd05cad
Klinshy <79143155+Klinshy@users.noreply.github.com>
2024-04-05 17:01:38 +0200

    updated offgrid objects
    silly work but this should have fixed it

---
+++ tilesets/offgrid under wall.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="offgrid under wall" tilewidth="32" tileheight="32" tilecount="3128" columns="46">
+ <image source="offgrid under wall.png" width="1472" height="2176"/>
+</tileset>
