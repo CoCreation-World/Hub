5b4f974caf20ef13ce76a8f0c04b3db9ff0d91a9
Klinshy <79143155+Klinshy@users.noreply.github.com>
2024-04-19 09:45:52 +0200

    added cuckoo timer

---
+++ tilesets/rotate_sprite.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="rotate_sprite" tilewidth="32" tileheight="32" tilecount="24" columns="6">
+ <image source="rotate_sprite.png" width="192" height="128"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="6" duration="1000"/>
+   <frame tileid="7" duration="250"/>
+   <frame tileid="8" duration="250"/>
+   <frame tileid="9" duration="250"/>
+   <frame tileid="10" duration="250"/>
+   <frame tileid="11" duration="250"/>
+   <frame tileid="18" duration="250"/>
+   <frame tileid="19" duration="250"/>
+   <frame tileid="20" duration="250"/>
+   <frame tileid="21" duration="250"/>
+   <frame tileid="22" duration="250"/>
+   <frame tileid="23" duration="250"/>
+  </animation>
+ </tile>
+</tileset>
