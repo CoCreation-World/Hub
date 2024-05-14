f0467924230cc2f06ccc38eef5fb2f5ce61bb923
Klinshy <79143155+Klinshy@users.noreply.github.com>
2024-05-14 14:40:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ WA.room.area.onEnter('showRoof').subscribe(() => {
     WA.room.showLayer('FG Exterior/Roof');
 });
 WA.room.area.onLeave('showRoof').subscribe(() => {
-    WA.room.hideLayer('FG Exterior/Roof');
+    WA.room.hideLayer('FG Exterior/Roof\nFG Exterior/RoofOpacity');
 });
 WA.room.area.onEnter('topRight').subscribe(() => {
     WA.room.showLayer('FG Exterior/RoofOpacity');
