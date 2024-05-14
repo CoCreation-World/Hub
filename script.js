46ca05fb8d4c643f0f74157eb327bd77a219759a
Klinshy <79143155+Klinshy@users.noreply.github.com>
2024-05-14 13:58:33 +0200

    addedLayerhideshow

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-WA.chat.sendChatMessage('Hello world', 'Mr Robot');
+WA.room.area.onEnter('showRoof').subscribe(() => {
+    WA.room.showLayer('Roof');
+});
+WA.room.area.onLeave('showRoof').subscribe(() => {
+    WA.room.hideLayer('Roof');
+});
