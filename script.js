a3b5b3a587f037c3dc1e0ba3633c47d2618f21d4
Klinshy <79143155+Klinshy@users.noreply.github.com>
2024-05-14 14:29:20 +0200

    test opacity

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ WA.room.area.onLeave('showRoof').subscribe(() => {
     WA.room.hideLayer('FG Exterior/Roof');
 });
 WA.room.area.onEnter('topRight').subscribe(() => {
-    WA.room.hideLayer('FG Exterior/Roof');
+    WA.room.showLayer('FG Exterior/RoofOpacity');
 });
 WA.room.area.onLeave('topRight').subscribe(() => {
-    WA.room.showLayer('FG Exterior/Roof');
+    WA.room.hideLayer('FG Exterior/RoofOpacity');
 });
