8bcfdb5ac41890b6a7d538991fffed7245a3cbc0
Kili S <79143155+klinshy@users.noreply.github.com>
2024-10-02 19:55:38 +0200

    new education modal

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,95 @@ console.log('Script started successfully');
 
 let currentPopup: any = undefined;
 
+WA.onInit().then(() => {
+    WA.controls.disablePlayerControls();
+    WA.controls.disableMicrophone();
+    WA.controls.disableWebcam();
+    WA.controls.disableWheelZoom();
+    WA.controls.disableRightClick();
+    WA.controls.disableInviteButton();
+    WA.controls.disableMapEditor();
+    WA.controls.disableRoomList();
+    WA.controls.disablePlayerProximityMeeting();
+    WA.controls.disableScreenSharing();
+
+    WA.ui.openPopup("popupRectangle", 'Welcome to \n CoCreation.World \n Would you like to experience the tutorial first?', [{
+        label: "👎 no",
+        className: "error",
+        callback: (popup) => {
+            // Close the popup when the "Close" button is pressed.
+            popup.close();
+            {
+                if (!WA.player.tags.includes("member")) {
+                    WA.controls.restorePlayerControls();
+                    WA.controls.restoreWheelZoom();
+                    WA.controls.restorePlayerProximityMeeting();
+                    WA.controls.restoreScreenSharing();
+                    WA.controls.restoreMicrophone();
+                    WA.controls.restoreWebcam();
+                    // WA.controls.restoreRightClick(); // currently deactivated. as this would allow users to sneak into closed areas.... its a WA bug. You cant walk into areas you dont have access to, using arrow keys. But you can using right click....
+                } else {
+                    WA.controls.restorePlayerControls();
+                    WA.controls.restoreMicrophone();
+                    WA.controls.restoreWebcam();
+                    WA.controls.restoreWheelZoom();
+                    WA.controls.restoreRightClick();
+                    WA.controls.restoreInviteButton();
+                    WA.controls.restoreMapEditor();
+                    WA.controls.restoreRoomList();
+                    WA.controls.restorePlayerProximityMeeting();
+                    WA.controls.restoreScreenSharing();
+                }
+            }
+        
+        },
+        
+    },
+    {
+        label: "👍 yes",
+        className: "warning",
+        callback: async (popup) => {
+            const playerName = WA.player.name;
+        const playerLanguage = WA.player.language;
+        const wokaurl = await WA.player.getWokaPicture();
+
+        var boturl = `https://chat.cocreation.world/ccw?playername=${encodeURIComponent(playerName)}&avatar=${encodeURIComponent(wokaurl)}&language=${playerLanguage}`;
+
+            WA.ui.modal.openModal({
+                title: "Welcome",
+                src: boturl,
+                allow: "fullscreen",
+                allowApi: true,
+                position: "center"
+            }) 
+            popup.close();
+            {
+                if (!WA.player.tags.includes("member")) {
+                    WA.controls.restorePlayerControls();
+                    WA.controls.restoreWheelZoom();
+                    WA.controls.restorePlayerProximityMeeting();
+                    WA.controls.restoreScreenSharing();
+                    WA.controls.restoreMicrophone();
+                    WA.controls.restoreWebcam();
+                    
+                    // WA.controls.restoreRightClick(); // currently deactivated. as this would allow users to sneak into closed areas.... its a WA bug. You cant walk into areas you dont have access to, using arrow keys. But you can using right click....
+                } else {
+                    WA.controls.restorePlayerControls();
+                    WA.controls.restoreMicrophone();
+                    WA.controls.restoreWebcam();
+                    WA.controls.restoreWheelZoom();
+                    WA.controls.restoreRightClick();
+                    WA.controls.restoreInviteButton();
+                    WA.controls.restoreMapEditor();
+                    WA.controls.restoreRoomList();
+                    WA.controls.restorePlayerProximityMeeting();
+                    WA.controls.restoreScreenSharing();
+                }
+            }
+        },
+        
+    }]    )
+});
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
@@ -83,61 +172,6 @@ WA.onInit().then(async () => {
             link: {
                 label: "CLICK HERE",
                 url: "https://world.cocreation.world/login"
-            }
-        });
-    }
-});
-WA.onInit().then(async () => {
-    // Check if the player has the "admin" tag
-    if (!WA.player.tags.includes("bot") && !WA.player.tags.includes("member")) {
-        const playerName = WA.player.name;
-        const playerLanguage = WA.player.language;
-        const wokaurl = await WA.player.getWokaPicture();
-
-        var boturl = `https://chat.cocreation.world/ccw?playername=${encodeURIComponent(playerName)}&avatar=${encodeURIComponent(wokaurl)}&language=${playerLanguage}`;
-
-        if (WA.player.tags.includes("admin")) {
-            boturl = `https://chat.cocreation.world/ccw?playername=${encodeURIComponent(playerName)}&avatar=${encodeURIComponent(wokaurl)}&admin=true&language=${playerLanguage}`;
-            
-        }
-
-        WA.controls.disablePlayerControls();
-        WA.controls.disableMicrophone();
-        WA.controls.disableWebcam();
-        WA.controls.disableWheelZoom();
-        WA.controls.disableRightClick();
-        WA.controls.disableInviteButton();
-        WA.controls.disableMapEditor();
-        WA.controls.disableRoomList();
-        WA.controls.disablePlayerProximityMeeting();
-        WA.controls.disableScreenSharing();
-
-        WA.ui.modal.openModal({
-            title: "Welcome",
-            src: boturl,
-            allow: "fullscreen",
-            allowApi: true,
-            position: "center"
-        }, () => {
-            if (!WA.player.tags.includes("member")) {
-                WA.controls.restorePlayerControls();
-                WA.controls.restoreWheelZoom();
-                WA.controls.restorePlayerProximityMeeting();
-                WA.controls.restoreScreenSharing();
-                WA.controls.restoreMicrophone();
-                WA.controls.restoreWebcam();
-                // WA.controls.restoreRightClick(); // currently deactivated. as this would allow users to sneak into closed areas.... its a WA bug. You cant walk into areas you dont have access to, using arrow keys. But you can using right click....
-            } else {
-                WA.controls.restorePlayerControls();
-                WA.controls.restoreMicrophone();
-                WA.controls.restoreWebcam();
-                WA.controls.restoreWheelZoom();
-                WA.controls.restoreRightClick();
-                WA.controls.restoreInviteButton();
-                WA.controls.restoreMapEditor();
-                WA.controls.restoreRoomList();
-                WA.controls.restorePlayerProximityMeeting();
-                WA.controls.restoreScreenSharing();
             }
         });
     }
