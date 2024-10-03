feb73f5ee0c285e6bbc967339089109bc2ba8149
Kili S <79143155+klinshy@users.noreply.github.com>
2024-10-03 13:21:19 +0200

    hotfix boturl

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
-import { bootstrapExtra, RemotePlayer } from "@workadventure/scripting-api-extra";
+import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
